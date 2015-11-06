5c580cdde8c6be8362f94a0a7d7ec54f1b173575
dendevjv <dendevjv@yandex.ru>
2015-11-06 14:58:31 +0300

    Added jsp-config to web.xml, created base.jspf with taglib, added c:redirect to index.jsp (TESTED)

---
+++ src/main/java/ru/dendevjv/customer_support/DummyServlet.java
@@ -0,0 +1,16 @@
+package ru.dendevjv.customer_support;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DummyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// empty
+	}
+
+}
