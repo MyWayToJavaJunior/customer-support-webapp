631617d0195e7f2fba4ed0ec0d64c040a260f0b3
dendevjv <dendevjv@yandex.ru>
2015-12-07 11:09:03 +0300

    Added SessionListener (listens when session is created, destroyed, id changed).

---
+++ src/main/java/ru/dendevjv/customer_support/SessionListener.java
@@ -0,0 +1,41 @@
+package ru.dendevjv.customer_support;
+
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionIdListener;
+import javax.servlet.http.HttpSessionListener;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@WebListener
+public class SessionListener implements HttpSessionListener, HttpSessionIdListener {
+    
+    private static final Logger log = LogManager.getLogger();
+    
+//    private SimpleDateFormat formatter = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss");
+
+    /* HttpSessionListener methods */
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        log.info("Session " + se.getSession().getId() + " created");
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        log.info("Session " + se.getSession().getId() + " destroyed");
+    }
+    
+    /* HttpSessionIdListener method */
+    @Override
+    public void sessionIdChanged(HttpSessionEvent event, String oldSessionId) {
+        log.info("Session " + oldSessionId + " changed to " + event.getSession().getId());
+    }
+
+//    private String date() {
+//        return this.formatter.format(new Date());
+//    }
+}
